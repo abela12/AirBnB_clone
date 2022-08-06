616f607f12cf11db6ace4d4a804aa4df2714bbc5
Robel-Ak <Robelamde21@gmail.com>
2022-08-06 23:25:31 +0300

    Airbnb

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,8 +2,8 @@
 """Unit test for the file storage class
 """
 import unittest
-# import json
-# import pep8
+import json
+import pep8
 from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
